28b82a2944f3a463ffc5ef833db324ca3c8fd239
Yuan <45984206+Yuan325@users.noreply.github.com>
2024-02-16 14:11:34 -0800

    chore(ci): update repo files (#12)
    * chore(ci): update repo files
    
    * Update version.py
    
    ---------
    
    Co-authored-by: Averi Kitsch <akitsch@google.com>

--- src/langchain_google_firestore/version.py
+++ src/langchain_google_firestore/version.py
@@ -12,7 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-docker:
-  image: gcr.io/cloud-devrel-public-resources/owlbot-python:latest
-
-begin-after-commit-hash: 70f7f0525414fe4dfeb2fc2e81546b073f83a621
+__version__ = "0.0.1"
