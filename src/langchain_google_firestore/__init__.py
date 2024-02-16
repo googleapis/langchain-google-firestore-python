677f7ac6787e185950d8d29ff92213bce720f888
Josue Urbina <jfurbina@google.com>
2024-02-16 22:54:08 +0000

    style: lint fix.

--- src/langchain_google_firestore/__init__.py
+++ src/langchain_google_firestore/__init__.py
@@ -12,7 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .version import __version__
 from langchain_google_firestore.document_loader import FirestoreLoader, FirestoreSaver
+
+from .version import __version__
 
 __all__ = ["__version__", "FirestoreLoader", "FirestoreSaver"]
