7683f071e561184b4531bb2a0bf0cde83bc59e6d
Averi Kitsch <akitsch@google.com>
2024-02-19 10:01:45 -0800

    Update publish-docs.sh

--- .kokoro/publish-docs.sh
+++ .kokoro/publish-docs.sh
@@ -27,7 +27,7 @@ python3 -m nox --version
 # build docs
 nox -s docs
 
-VERSION=$(python -c "import langchain_google_cloud_sql_pg;print(langchain_google_cloud_sql_pg.__version__)")
+VERSION=$(python -c "import langchain_google_firestore;print(langchain_google_firestore.__version__)")
 
 # create metadata
 python3 -m docuploader create-metadata \
