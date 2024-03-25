f61fd80d56669ac4f33e56f1c47c83e8c02b6a63
Averi Kitsch <akitsch@google.com>
2024-03-25 14:41:41 -0700

    fix: update user agent (#37)
    * fix: udpate user agent
    
    * Update common.py
    
    * Update chat_message_history.py
    
    * Update chat_message_history.py
    
    * fix lint

---
+++ src/langchain_google_firestore/common.py
@@ -0,0 +1,34 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from google.cloud import firestore  # type: ignore
+from google.cloud.firestore_v1.services.firestore.transports.base import (  # type: ignore
+    DEFAULT_CLIENT_INFO,
+)
+
+
+def client_with_user_agent(
+    client: firestore.Client | None, user_agent: str
+) -> firestore.Client:
+    client_info = DEFAULT_CLIENT_INFO
+    client_info.user_agent = user_agent
+    if not client:
+        client = firestore.Client(client_info=client_info)
+    client_agent = client._client_info.user_agent
+    if not client_agent:
+        client._client_info.user_agent = user_agent
+    elif user_agent not in client_agent:
+        client._client_info.user_agent = " ".join([user_agent, client_agent])
+    return client
+    return client
