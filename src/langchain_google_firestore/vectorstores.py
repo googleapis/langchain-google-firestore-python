b8f144bdb809448e8c9661ff8c6ebd64e62823c5
klmilam <38926902+klmilam@users.noreply.github.com>
2024-05-28 17:55:52 -0400

    chore: Improve vector store documentation (#70)
    * Switch embedding argument to embedding_service
    
    * Use COLLECTION_NAME and add index note
    
    * source -> collection in docstring

--- src/langchain_google_firestore/vectorstores.py
+++ src/langchain_google_firestore/vectorstores.py
@@ -57,9 +57,9 @@ class FirestoreVectorStore(VectorStore):
         """Constructor for FirestoreVectorStore.
 
         Args:
-            source (CollectionReference | str): The source collection or document
+            collection (CollectionReference | str): The source collection or document
             reference to store the data.
-            embedding (Embeddings): The embeddings to use for the vector store.
+            embedding_service (Embeddings): The embeddings to use for the vector store.
             client (Optional[Client]): The Firestore client to use. If not provided,
             a new client will be created.
             content_field (str): The field name to store the content data.
